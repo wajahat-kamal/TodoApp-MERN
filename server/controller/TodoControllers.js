3828afedb1fd4c3044d9d2dcff2131b279f69c35
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-12-24 15:02:20 +0500

    Enhance createTodo function with error handling and response structure; switch to using the correct Todo model import.

--- server/controller/TodoControllers.js
+++ server/controller/TodoControllers.js
@@ -1,17 +1,38 @@
+import Todo from "../model/TodoSchema";
 
 export const getTodo = (req, res) => {
  
 }
 
-export const createTodo = (req, res) => {
-    const {title, description, completed} = req.body;
+import Todo from "../models/todoModel.js";
+
+export const createTodo = async (req, res) => {
+  try {
+    const { title, description, completed } = req.body;
+
     if (!title) {
-        return res.status(400).json({
-            message: "Please Enter a Title",
-            success: false
-        })
+      return res.status(400).json({
+        success: false,
+        message: "Please Enter a Title",
+      });
     }
-}
+
+    const todo = await Todo.create({ title, description, completed });
+
+    res.status(201).json({
+      success: true,
+      message: "Todo is created",
+      todo,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server Problem in create Todo",
+      error: error.message,
+    });
+  }
+};
+
 
 export const updateTodo = (req, res) => {
 
