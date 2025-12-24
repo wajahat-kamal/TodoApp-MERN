04d77c2cd92bad51399d80793787df736fda29b4
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-12-24 14:58:25 +0500

    Refactor TodoControllers and remove Todo model file

--- server/controller/TodoControllers.js
+++ server/controller/TodoControllers.js
@@ -1,10 +1,16 @@
 
 export const getTodo = (req, res) => {
-
+ 
 }
 
 export const createTodo = (req, res) => {
-
+    const {title, description, completed} = req.body;
+    if (!title) {
+        return res.status(400).json({
+            message: "Please Enter a Title",
+            success: false
+        })
+    }
 }
 
 export const updateTodo = (req, res) => {
