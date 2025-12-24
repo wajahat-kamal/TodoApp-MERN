00088fdc8b9015efda7a98eec5cc776331b6d4c1
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-12-24 15:22:38 +0500

    Implement deleteTodo function with error handling and remove TodoSchema model file

--- server/controllers/TodoController.js
+++ server/controllers/TodoController.js
@@ -16,8 +16,6 @@ export const getTodos = async (req, res) => {
   }
 };
 
-
-
 export const createTodo = async (req, res) => {
   try {
     const { title, description, completed } = req.body;
@@ -45,11 +43,28 @@ export const createTodo = async (req, res) => {
   }
 };
 
+export const updateTodo = async (req, res) => {};
 
-export const updateTodo = (req, res) => {
+export const deleteTodo = async (req, res) => {
+  try {
+    const deletedTodo = await Todo.findByIdAndDelete(req.params.id);
 
-}
+    if (!deletedTodo) {
+      return res.status(404).json({
+        success: false,
+        message: "Todo not found",
+      });
+    }
 
-export const deleteTodo = (req, res) => {
-
-}
+    res.status(200).json({
+      success: true,
+      message: "Todo deleted successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server Problem in deleting todo",
+      error: error.message,
+    });
+  }
+};
